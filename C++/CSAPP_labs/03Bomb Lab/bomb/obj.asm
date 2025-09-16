bb33abbe8556e29139037b5edf29363cc5708795
LiuWen <162258246+houli123@users.noreply.github.com>
2025-09-16 18:40:48 +0800

    update chapter 3

--- C++/CSAPP_labs/03Bomb Lab/bomb/obj.asm
+++ C++/CSAPP_labs/03Bomb Lab/bomb/obj.asm
@@ -355,63 +355,63 @@ Disassembly of section .text:
 0000000000400efc <phase_2>:
   400efc:       55                      push   %rbp
   400efd:       53                      push   %rbx
-  400efe:       48 83 ec 28             sub    $0x28,%rsp
+  400efe:       48 83 ec 28             sub    $0x28,%rsp  # 40
   400f02:       48 89 e6                mov    %rsp,%rsi
   400f05:       e8 52 05 00 00          call   40145c <read_six_numbers>
-  400f0a:       83 3c 24 01             cmpl   $0x1,(%rsp)
-  400f0e:       74 20                   je     400f30 <phase_2+0x34>
+  400f0a:       83 3c 24 01             cmpl   $0x1,(%rsp)  # 第一个比较是1
+  400f0e:       74 20                   je     400f30 <phase_2+0x34>  # 第一次跳
   400f10:       e8 25 05 00 00          call   40143a <explode_bomb>
   400f15:       eb 19                   jmp    400f30 <phase_2+0x34>
-  400f17:       8b 43 fc                mov    -0x4(%rbx),%eax
-  400f1a:       01 c0                   add    %eax,%eax
-  400f1c:       39 03                   cmp    %eax,(%rbx)
-  400f1e:       74 05                   je     400f25 <phase_2+0x29>
+  400f17:       8b 43 fc                mov    -0x4(%rbx),%eax  # 第一个数移动；第三个数移动；第四
+  400f1a:       01 c0                   add    %eax,%eax # 这里得到2；得到4；得到8（从这里就知道规律了，每次都是上一个数相加）
+  400f1c:       39 03                   cmp    %eax,(%rbx)  # 记得现在rbx是第二个数，和刚刚的2比较
+  400f1e:       74 05                   je     400f25 <phase_2+0x29>  # 第三次跳；第五次跳
   400f20:       e8 15 05 00 00          call   40143a <explode_bomb>
-  400f25:       48 83 c3 04             add    $0x4,%rbx
-  400f29:       48 39 eb                cmp    %rbp,%rbx
-  400f2c:       75 e9                   jne    400f17 <phase_2+0x1b>
+  400f25:       48 83 c3 04             add    $0x4,%rbx  # rbx现在是第三个数；第四个数
+  400f29:       48 39 eb                cmp    %rbp,%rbx  # 按理应该不想等
+  400f2c:       75 e9                   jne    400f17 <phase_2+0x1b>  # 第四次跳；第六次跳
   400f2e:       eb 0c                   jmp    400f3c <phase_2+0x40>
-  400f30:       48 8d 5c 24 04          lea    0x4(%rsp),%rbx
+  400f30:       48 8d 5c 24 04          lea    0x4(%rsp),%rbx  # rbp指向栈底，rbx指向第二个数，这里是被调用者保存寄存器
   400f35:       48 8d 6c 24 18          lea    0x18(%rsp),%rbp
-  400f3a:       eb db                   jmp    400f17 <phase_2+0x1b>
+  400f3a:       eb db                   jmp    400f17 <phase_2+0x1b> # 第二次跳
   400f3c:       48 83 c4 28             add    $0x28,%rsp
   400f40:       5b                      pop    %rbx
   400f41:       5d                      pop    %rbp
   400f42:       c3                      ret
 
 0000000000400f43 <phase_3>:
-  400f43:       48 83 ec 18             sub    $0x18,%rsp
-  400f47:       48 8d 4c 24 0c          lea    0xc(%rsp),%rcx
-  400f4c:       48 8d 54 24 08          lea    0x8(%rsp),%rdx
-  400f51:       be cf 25 40 00          mov    $0x4025cf,%esi
+  400f43:       48 83 ec 18             sub    $0x18,%rsp  # rsp - 24得出是六个数字（下面看出一个占4字节）
+  400f47:       48 8d 4c 24 0c          lea    0xc(%rsp),%rcx # (rsp - 12)移到rcx上；第三个数
+  400f4c:       48 8d 54 24 08          lea    0x8(%rsp),%rdx # (rsp - 8)移到rdx上；第二个数
+  400f51:       be cf 25 40 00          mov    $0x4025cf,%esi  # 查看了地址，是两个%d
   400f56:       b8 00 00 00 00          mov    $0x0,%eax
-  400f5b:       e8 90 fc ff ff          call   400bf0 <__isoc99_sscanf@plt>
-  400f60:       83 f8 01                cmp    $0x1,%eax
-  400f63:       7f 05                   jg     400f6a <phase_3+0x27>
+  400f5b:       e8 90 fc ff ff          call   400bf0 <__isoc99_sscanf@plt> # 函数scanf的返回值应该是读入进去的个数吧？
+  400f60:       83 f8 01                cmp    $0x1,%eax  # 如果小于两个数就爆炸
+  400f63:       7f 05                   jg     400f6a <phase_3+0x27>  # 第一次跳
   400f65:       e8 d0 04 00 00          call   40143a <explode_bomb>
-  400f6a:       83 7c 24 08 07          cmpl   $0x7,0x8(%rsp)
-  400f6f:       77 3c                   ja     400fad <phase_3+0x6a>
-  400f71:       8b 44 24 08             mov    0x8(%rsp),%eax
-  400f75:       ff 24 c5 70 24 40 00    jmp    *0x402470(,%rax,8)
-  400f7c:       b8 cf 00 00 00          mov    $0xcf,%eax
-  400f81:       eb 3b                   jmp    400fbe <phase_3+0x7b>
-  400f83:       b8 c3 02 00 00          mov    $0x2c3,%eax
+  400f6a:       83 7c 24 08 07          cmpl   $0x7,0x8(%rsp)  # 这里是设定switch的范围是1-7，7是相对于减去最小值来实现规格化
+  400f6f:       77 3c                   ja     400fad <phase_3+0x6a> # 第二次跳
+  400f71:       8b 44 24 08             mov    0x8(%rsp),%eax # 第二个数移动到eax
+  400f75:       ff 24 c5 70 24 40 00    jmp    *0x402470(,%rax,8)  # 跳转表，也就是参数1选择在1-7范围，然后对应参数2的每个都不同
+  400f7c:       b8 cf 00 00 00          mov    $0xcf,%eax   # case 1
+  400f81:       eb 3b                   jmp    400fbe <phase_3+0x7b> 
+  400f83:       b8 c3 02 00 00          mov    $0x2c3,%eax  # case 2
   400f88:       eb 34                   jmp    400fbe <phase_3+0x7b>
-  400f8a:       b8 00 01 00 00          mov    $0x100,%eax
+  400f8a:       b8 00 01 00 00          mov    $0x100,%eax  # 3
   400f8f:       eb 2d                   jmp    400fbe <phase_3+0x7b>
-  400f91:       b8 85 01 00 00          mov    $0x185,%eax
+  400f91:       b8 85 01 00 00          mov    $0x185,%eax # 4
   400f96:       eb 26                   jmp    400fbe <phase_3+0x7b>
-  400f98:       b8 ce 00 00 00          mov    $0xce,%eax
+  400f98:       b8 ce 00 00 00          mov    $0xce,%eax # 5
   400f9d:       eb 1f                   jmp    400fbe <phase_3+0x7b>
-  400f9f:       b8 aa 02 00 00          mov    $0x2aa,%eax
+  400f9f:       b8 aa 02 00 00          mov    $0x2aa,%eax # 6
   400fa4:       eb 18                   jmp    400fbe <phase_3+0x7b>
-  400fa6:       b8 47 01 00 00          mov    $0x147,%eax
+  400fa6:       b8 47 01 00 00          mov    $0x147,%eax # 7
   400fab:       eb 11                   jmp    400fbe <phase_3+0x7b>
   400fad:       e8 88 04 00 00          call   40143a <explode_bomb>
   400fb2:       b8 00 00 00 00          mov    $0x0,%eax
   400fb7:       eb 05                   jmp    400fbe <phase_3+0x7b>
   400fb9:       b8 37 01 00 00          mov    $0x137,%eax
-  400fbe:       3b 44 24 0c             cmp    0xc(%rsp),%eax
+  400fbe:       3b 44 24 0c             cmp    0xc(%rsp),%eax  # 跳到这，判断是不是207
   400fc2:       74 05                   je     400fc9 <phase_3+0x86>
   400fc4:       e8 71 04 00 00          call   40143a <explode_bomb>
   400fc9:       48 83 c4 18             add    $0x18,%rsp
@@ -443,23 +443,23 @@ Disassembly of section .text:
 
 000000000040100c <phase_4>:
   40100c:       48 83 ec 18             sub    $0x18,%rsp
-  401010:       48 8d 4c 24 0c          lea    0xc(%rsp),%rcx
+  401010:       48 8d 4c 24 0c          lea    0xc(%rsp),%rcx 
   401015:       48 8d 54 24 08          lea    0x8(%rsp),%rdx
   40101a:       be cf 25 40 00          mov    $0x4025cf,%esi
   40101f:       b8 00 00 00 00          mov    $0x0,%eax
   401024:       e8 c7 fb ff ff          call   400bf0 <__isoc99_sscanf@plt>
   401029:       83 f8 02                cmp    $0x2,%eax
   40102c:       75 07                   jne    401035 <phase_4+0x29>
-  40102e:       83 7c 24 08 0e          cmpl   $0xe,0x8(%rsp)
+  40102e:       83 7c 24 08 0e          cmpl   $0xe,0x8(%rsp)  #
   401033:       76 05                   jbe    40103a <phase_4+0x2e>
   401035:       e8 00 04 00 00          call   40143a <explode_bomb>
-  40103a:       ba 0e 00 00 00          mov    $0xe,%edx
-  40103f:       be 00 00 00 00          mov    $0x0,%esi
+  40103a:       ba 0e 00 00 00          mov    $0xe,%edx  # edx = 14
+  40103f:       be 00 00 00 00          mov    $0x0,%esi # esi = 0
   401044:       8b 7c 24 08             mov    0x8(%rsp),%edi
   401048:       e8 81 ff ff ff          call   400fce <func4>
-  40104d:       85 c0                   test   %eax,%eax
+  40104d:       85 c0                   test   %eax,%eax # 需要==0
   40104f:       75 07                   jne    401058 <phase_4+0x4c>
-  401051:       83 7c 24 0c 00          cmpl   $0x0,0xc(%rsp)
+  401051:       83 7c 24 0c 00          cmpl   $0x0,0xc(%rsp)  
   401056:       74 05                   je     40105d <phase_4+0x51>
   401058:       e8 dd 03 00 00          call   40143a <explode_bomb>
   40105d:       48 83 c4 18             add    $0x18,%rsp
